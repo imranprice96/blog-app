0d0a9c4894db3bbbd08f697d0b106abf00ea60ed
pricei <pricei@Xubuntu.myguest.virtualbox.org>
2024-02-02 17:32:25 +1300

    Adding test api request for posts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
-  return (
-    <div className="container">
-      <div className="main-body">a</div>
-    </div>
-  );
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/posts`, {
+          method: "GET",
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+        setPosts(data.posts);
+        console.log(data.posts);
+      } catch (err) {
+        setError(err.message);
+        setPosts(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  if (error) {
+    return (
+      <div className="container">
+        <div className="main-body">
+          <p>Error loading posts: {error}</p>
+        </div>
+      </div>
+    );
+  } else if (loading) {
+    return (
+      <div className="container">
+        <div className="main-body">
+          <p>Loading posts...</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="main-body">
+          {posts.map((post) => (
+            <div key={post._id} className="post-container">
+              <h3>{post.title}</h3>
+              <p>{post.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function Post(postid) {
+  const [comments, setComments] = useState();
+  return <div className="post-container"></div>;
+}
+
+export default Post;
