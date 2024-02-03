710bdd52316bd48affd65af6b3671572a02f5894
pricei <pricei@Xubuntu.myguest.virtualbox.org>
2024-02-03 17:33:01 +1300

    Adding comment form inputs

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,9 +4,15 @@ import { useState } from "react";
 import { useCollapse } from "react-collapsed";
 
 function Comments({ comments, count }) {
+  const [data, setData] = useState({});
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({
     duration: 200,
   });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="comment-container">
       <div className="add-comment">
@@ -14,9 +20,31 @@ function Comments({ comments, count }) {
           <b>Add Comment</b>
           <b id="down-arrow">&#8964;</b>
         </span>
-        <form className="add-comment-form" {...getCollapseProps()}>
-          <p>Username</p>
-          <p>text</p>
+        <form
+          className="add-comment-form"
+          {...getCollapseProps()}
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="form-username"
+            name="username"
+            placeholder="Name"
+            type="text"
+            required
+            maxLength={100}
+            onChange={(e) => setData({ ...data, username: e.target.value })}
+          />
+          <textarea
+            className="form-text"
+            name="text"
+            placeholder="Max 512 characters"
+            required
+            rows={5}
+            maxLength={512}
+          />
+          <span className="form-span">
+            <button className="form-submit">Submit</button>
+          </span>
         </form>
       </div>
 
